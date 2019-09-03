7bb924817453267c91e9af435240e1d54b3fe91f
vinicius-sabadim <vinicius.sabadim@gmail.com>
2019-09-03 19:55:19 +0200

    Updates

---
+++ precache-manifest.1755b8ed045d9ebc2a0fe0cb0d3d19ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9cfa0370a2df86a9f05566e888e3515",
+    "url": "/findhotel-code-challenge/index.html"
+  },
+  {
+    "revision": "6f09a3690023d67f2142",
+    "url": "/findhotel-code-challenge/static/css/main.80e8b590.chunk.css"
+  },
+  {
+    "revision": "491e165e82ed71fe01b6",
+    "url": "/findhotel-code-challenge/static/js/2.7b6ec7b1.chunk.js"
+  },
+  {
+    "revision": "6f09a3690023d67f2142",
+    "url": "/findhotel-code-challenge/static/js/main.3ee732cc.chunk.js"
+  },
+  {
+    "revision": "f849d14842c17ca7a80c",
+    "url": "/findhotel-code-challenge/static/js/runtime~main.87aa3b7e.js"
+  }
+]);
